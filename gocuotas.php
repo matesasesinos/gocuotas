8bd9e6feec0c7bca876bea2ef219accebf498fe0
Juan Iriart <jiriart@preminens.com.ar>
2025-05-15 12:01:56 -0300

    added updater

--- gocuotas.php
+++ gocuotas.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: Go Cuotas
- * Version: 1.3.1
+ * Version: 1.3.2
  * Author: Juan Iriart
  * Text Domain: gocuotas
  * Description: Plugin para integración de Go Cuotas en WooCommerce
@@ -12,7 +12,18 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('GC_VERSION', '1.3.1');
+require_once __DIR__.'/updater/plugin-update-checker.php';;
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/matesasesinos/gocuotas',
+	__FILE__,
+	'gocuotas'
+);
+
+$myUpdateChecker->setBranch('stable');
+
+define('GC_VERSION', '1.3.2');
 
 require_once plugin_dir_path(__FILE__) . 'includes/helpers/class-helper.php';
 
